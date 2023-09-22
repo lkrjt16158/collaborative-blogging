73d417712bb233cd4a2ac975a840af97eead88c3
lkrjt16158 <lkrjt16158@gmail.com>
2023-09-23 01:50:54 +0530

    Initial commit

---
+++ src/main/java/com/lk/collaborative/blogging/CollaborativeBloggingApplication.java
@@ -0,0 +1,13 @@
+package com.lk.collaborative.blogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeBloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeBloggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lk/collaborative/blogging/CollaborativeBloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lk.collaborative.blogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborativeBloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
